d14595b5e354e1b8af92430c5de6c6693e4b3d68
Ali Hourag <ali.hourag@gmail.com>
2023-10-30 12:55:35 +0100

    feat(change cursor form)

--- src/page/mainPage/MainPage.tsx
+++ src/page/mainPage/MainPage.tsx
@@ -1,10 +1,11 @@
+import Menu from "../../components/menu/Menu";
 import styles from "./mainPage.module.css";
 
 
 const MainPage = () => {
     return (
         <main className={styles.container}>
-
+            <Menu />
         </main>
     )
 }
