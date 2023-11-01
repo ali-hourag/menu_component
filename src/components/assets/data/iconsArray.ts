4272e38476f7c9b806c2e81789f540161f7b15e9
Ali Hourag <ali.hourag@gmail.com>
2023-11-01 12:52:53 +0100

    feat(add slider when mouse is over menu icons)

---
+++ src/components/assets/data/iconsArray.ts
@@ -0,0 +1,34 @@
+
+
+type IconArrayType = {
+    iconName: string,
+    iconLabel: string
+}
+
+
+export const iconsArray: IconArrayType[] = [
+    {
+        iconName: "solar:suitcase-bold",
+        iconLabel: "My Offers"
+    },
+    {
+        iconName: "icon-park-solid:notes",
+        iconLabel: "User's Task"
+    },
+    {
+        iconName: "ic:round-discount",
+        iconLabel: "Product Discounts"
+    },
+    {
+        iconName: "game-icons:graduate-cap",
+        iconLabel: "Case Study"
+    },
+    {
+        iconName: "mdi:security-lock",
+        iconLabel: "Security"
+    },
+    {
+        iconName: "mdi:thunder-circle",
+        iconLabel: "Your Ability"
+    }
+]
--- src/components/menuItems/MenuItems.tsx
+++ src/components/menuItems/MenuItems.tsx
@@ -2,23 +2,45 @@ import { Icon } from '@iconify/react';
 import styles from "./menuItems.module.css";
 import { useThemeModeContext } from '../../utils/hooks/useThemeModeContext';
 import { ThemeModeType } from '../../context/ThemeModeContextProvider';
+import { iconsArray } from '../assets/data/iconsArray';
 
 const MenuItems = () => {
 
     const { themeMode } = useThemeModeContext();
 
-    const iconsArray = ["solar:suitcase-bold", "icon-park-solid:notes", "ic:round-discount",
-        "game-icons:graduate-cap", "mdi:security-lock", "mdi:thunder-circle"]
+    const handleMouseOverMenuIcon = (iconPosition: number, isEntering: boolean) => {
+        const iconLabelContainer: (HTMLDivElement | null) = document.querySelector(`#menu-icons_entry-${iconPosition + 1} #icon-label-container`);
+        const iconLabel: (HTMLDivElement | null) = document.querySelector(`#menu-icons_entry-${iconPosition + 1} #icon-label`);
+        if (iconLabelContainer && iconLabel) {
+            if (isEntering) {
+                iconLabelContainer.style.opacity = "1";
+                iconLabel.style.left = "10px";
+            } else {
+                iconLabelContainer.style.opacity = "0";
+                iconLabel.style.left = "-200px";
+            }
+        }
+    }
 
     return (
         <div className={`${styles.container} ${themeMode === ThemeModeType.LIGHT_MODE ? "border-bottom_light-mode" : ""} `}>
-            <h5 className={`${styles.title} ${themeMode === ThemeModeType.LIGHT_MODE ? "menu-msgs-light-mode" : ""}`}>
-                MENU
-            </h5>
+            <div className={styles.titleContainer}>
+                <h5 className={`${styles.title} ${themeMode === ThemeModeType.LIGHT_MODE ? "menu-msgs-light-mode" : ""}`}>
+                    MENU
+                </h5>
+            </div>
             {iconsArray.map((icon, index) => (
-                <div className={styles.menuIconContainer} key={index}>
-                    <Icon icon={icon} hFlip={true}
-                        className={`${styles.menuIcon} ${themeMode === ThemeModeType.LIGHT_MODE ? "menu-msgs-light-mode" : ""}`} />
+                <div id={`menu-icons_entry-${index + 1}`} className={styles.menuIconContainer} key={index}>
+                    <div className={styles.iconContainer}>
+                        <Icon icon={icon.iconName} hFlip={true}
+                            className={`${styles.menuIcon} ${themeMode === ThemeModeType.LIGHT_MODE ? "menu-msgs-light-mode" : ""}`}
+                            onMouseEnter={() => handleMouseOverMenuIcon(index, true)}
+                            onMouseLeave={() => handleMouseOverMenuIcon(index, false)}
+                        />
+                    </div>
+                    <div id="icon-label-container" className={styles.iconLabelContainer}>
+                        <p id="icon-label" className={styles.iconLabel}>{icon.iconLabel}</p>
+                    </div>
                 </div>
             ))}
         </div>
